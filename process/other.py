5e492e040d86a06d35e6cb7b42cc89a4b9c4bae2
xingpuwang <xingpuwang@gmail.com>
2023-05-24 16:50:26 +0800

    Update

--- process/other.py
+++ process/other.py
@@ -4,9 +4,7 @@ import pandas as pd
 from glob import glob
 from collections import Counter
 
-sys.path.append('..')
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from graph import *
 from ocr import *
 
-if __name__ == '__main__':
-    print(os.getcwd())
